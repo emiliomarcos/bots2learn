3b129a6b0815cfe36195d8580c869d91adce305e
Emilio Marcos <emilio7marcos@gmail.com>
2023-08-13 08:27:08 -0600

    set up routes

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Landing } from './Landing'
+export { default as Navbar } from './Navbar'
+export { default as Questionizer } from './Questionizer'
+export { default as Summarizer } from './Summarizer'
