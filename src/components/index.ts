71b45b638c4257c6bd85a17a804bbb7ed98b3f4f
Emilio Marcos <emilio7marcos@gmail.com>
2023-08-13 09:17:50 -0600

    organized design

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,4 @@
 export { default as Landing } from './Landing'
-export { default as Navbar } from './Navbar'
+export { default as Navbar } from './Navbar/Navbar'
 export { default as Questionizer } from './Questionizer'
 export { default as Summarizer } from './Summarizer'
